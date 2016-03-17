53f45f828378d12b09dca742274ce5f52442e869
Emily Quill <emilyquill@outlook.com>
2016-03-18 00:16:31 +1100

    Adding dog database

---
+++ EmilyQuill/week_04/Dogs-Database/dogs.sql
@@ -0,0 +1,39 @@
+CREATE TABLE dogs (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  breed TEXT,
+  family TEXT,
+  life_span TEXT,
+  image TEXT,
+  more_info TEXT
+);
+
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Afghan Hound
+','Hound','10 to 12 years','http://www.dogtime.com/assets/uploads/2011/01/file_23004_afghan-hound-460x290.jpg','http://dogtime.com/dog-breeds/afghan-hound');
+
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Australian Terrier
+','Terrier','Up to 15 years','http://www.dogtime.com/assets/uploads/2011/01/file_23054_australian-terrier-460x290.jpg', 'http://dogtime.com/dog-breeds/australian-terrier');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Beagle','Hound','10 to 15 years','http://www.dogtime.com/assets/uploads/2011/01/file_23012_beagle-460x290.jpg','http://dogtime.com/dog-breeds/beagle');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Border Terrier
+','Terrier','12 to 15 years','http://www.dogtime.com/assets/uploads/2011/01/file_23058_border-terrier-460x290.jpg','http://dogtime.com/dog-breeds/border-terrier');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Boykin Spaniel
+','Sporting','10 to 12 years','http://www.dogtime.com/assets/uploads/2011/01/file_23242_boykin-spaniel-460x290.jpg','http://dogtime.com/dog-breeds/boykin-spaniel');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Bullmastiff','Working','8 to 10 years','http://www.dogtime.com/assets/uploads/2011/01/file_22918_bullmastiff-460x290.jpg','http://dogtime.com/dog-breeds/bullmastiff');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Cockapoo','Hybrid','12 to 15 years','http://www.dogtime.com/assets/uploads/2011/01/file_23218_cockapoo-dog-breed.jpg','http://dogtime.com/dog-breeds/cockapoo');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Dalmatian','Companion','13 to 16 years','http://www.dogtime.com/assets/uploads/2011/01/file_23146_dalmatian-460x290.jpg','http://dogtime.com/dog-breeds/dalmatian');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Doberman Pinscher
+','Working','10 to 13 years','http://www.dogtime.com/assets/uploads/2011/01/file_22920_doberman-pinscher-460x290.jpg','http://dogtime.com/dog-breeds/doberman-pinscher');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Dachshund
+','Hound','12 to 15 years','http://www.dogtime.com/assets/uploads/2011/01/file_23020_dachshund-dog-breed-460x290.jpg','http://dogtime.com/dog-breeds/dachshund');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Field Spaniel
+','Sporting','10 to 12 years','http://www.dogtime.com/assets/uploads/2011/01/file_22972_field-spaniel-460x290.jpg','http://dogtime.com/dog-breeds/field-spaniel');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('German Shorthaired Pointer
+','Sporting','12 to 15 years','http://www.dogtime.com/assets/uploads/2011/01/file_22976_german-shorthaired-pointer-460x290.jpg','http://dogtime.com/dog-breeds/german-shorthaired-pointer');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Irish Setter
+','Sporting','11 to 15 years','http://www.dogtime.com/assets/uploads/2011/01/file_22984_irish-setter-460x290.jpg','http://dogtime.com/dog-breeds/irish-setter');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('
+
+Nova Scotia Duck Tolling Retriever','Sporting','10 to 14 years','http://www.dogtime.com/assets/uploads/2011/01/file_22990_nova-scotia-duck-tolling-retriever-460x290.jpg','http://dogtime.com/dog-breeds/nova-scotia-duck-tolling-retriever');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('Vizsla
+','Sporting','10 to 14 years','http://www.dogtime.com/assets/uploads/2011/01/file_22996_vizsla-460x290.jpg','http://dogtime.com/dog-breeds/vizsla');
+INSERT INTO dogs (breed, family, life_span, image, more_info) VALUES ('West Highland White Terrier
+','Terrier','12 to 16 years','http://www.dogtime.com/assets/uploads/2011/01/file_23092_west-highland-white-terrier-460x290.jpg','http://dogtime.com/dog-breeds/west-highland-white-terrier');
