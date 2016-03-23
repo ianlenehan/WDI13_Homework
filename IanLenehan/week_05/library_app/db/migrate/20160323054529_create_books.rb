7a2ab7e6f282d7bf546bc8225e2fa9ce5f5aa6cb
“Ian <ianlenehan@gmail.com>
2016-03-24 09:36:11 +1100

    completed Library app

---
+++ IanLenehan/week_05/library_app/db/migrate/20160323054529_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.text :title
+      t.text :year
+      t.text :genre
+      t.text :cover
+      t.text :synopsis
+      t.timestamps
+    end
+  end
+end
