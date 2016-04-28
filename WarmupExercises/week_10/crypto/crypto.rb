82b1fff6976de46ee168f60d83241df51fa7fc5d
Chris Barnaby <cjbarnaby@gmail.com>
2016-04-28 13:03:18 +1000

    Update crypto.rb

--- WarmupExercises/week_10/crypto/crypto.rb
+++ WarmupExercises/week_10/crypto/crypto.rb
@@ -15,23 +15,29 @@ class Crypto
     end
 
     def size
-        # We want to try to create a perfect square of characters. Take the length of the normalized string, find the square root of that, then round it up to the nearest whole number.
+        # We want to try to create a perfect square of characters. Take the length of the normalized string, find the square root of that, then round it up to the nearest whole number. 
         Math.sqrt(normalize_plaintext.length).ceil
     end
 
     def plaintext_segments
-        # .scan returns an array of the matches. The regex says "find me between 1 and [square size] of anything and push that to an array". The scan method will continue through to the end of the string and give us something like: [finet, henar, eyouh, appyl, ukeif, thati, syour, realn ame]
+        # .scan returns an array of the matches. The regex says "find me between 1 and [square size] of anything and push that to an array". The scan method will continue finding matches (which will just be groups of any characters)  through to the end of the string and give us something like: [finet, henar, eyouh, appyl, ukeif, thati, syour, realn ame]
         normalize_plaintext.scan(/.{1,#{size}}/)
     end
 
     def ciphertext
+        # Create an empty array and...
         cipher_text = []
+        # ... for each of the segments in plaintext_segments...
         plaintext_segments.each do |segment|
+            # ... grab each character in the segment, along with its index and...
             segment.chars.each_with_index do |letter, i|
+                # ... if there is no i'th element in the ciphertext array, create one, and...
                 cipher_text[i] ||= []
+                # ...append the letter to that element
                 cipher_text[i] << letter
             end
         end
+        # Flatten the resulting array
     cipher_text.join
     end
 
@@ -43,10 +49,12 @@ class Crypto
     # end
 
     def normalize_ciphertext
+        # Go through the ciphertext string and put a space between every 5th character.
         ciphertext.scan(/.{1,5}/).join(" ")
     end
 
 end
 
 crypto = Crypto.new("Fine then. Are you happy, Luke? IF THAT IS YOUR REAL NAME!")
+
 puts crypto.normalize_ciphertext
